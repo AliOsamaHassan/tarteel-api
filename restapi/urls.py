34da1a3441fb8e7c3f692f5cbacb1b3bd20da01c
Anas Abou Allaban <anas@abouallaban.info>
2019-06-01 19:46:18 -0400

    Added next/prev ayahs

--- restapi/urls.py
+++ restapi/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('get_ayah/', views.site.GetAyah.as_view(), name='get_ayah'),
     path('index/', views.site.Index.as_view(), name='api_index'),
     path('about/', views.site.About.as_view(), name='api_about'),
-    path('surah/<str:surah_num>/', views.site.GetSurah.as_view(), name='get_surah'),
+    path('surah/<int:surah_num>/', views.site.GetSurah.as_view(), name='get_surah'),
     re_path(r'^profile/(?P<session_key>[\w-]+)/', views.site.Profile.as_view(),
             name='profile_api'),
     path('download-audio/', views.site.DownloadAudio.as_view()),
